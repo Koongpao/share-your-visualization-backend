f1e4943c0c3e88994d15826f5dc2fb1285c9536c
totoro <shockterraz@gmail.com>
2023-12-10 22:56:45 +0700

    Config for vercel

--- src/routes/visualization.js
+++ src/routes/visualization.js
@@ -2,11 +2,11 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("", (req, res) => {
+router.get("/", (req, res) => {
   res.send([{ item: "test", quantity: 2 }]);
 });
 
-router.post("", (req, res) => {
+router.post("/", (req, res) => {
   console.log(req.body);
   res.status(201).send("test");
 });
