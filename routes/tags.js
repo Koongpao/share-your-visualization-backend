d7579cae6837498a16d358df9108d0ddbc2e3bcb
totoro <shockterraz@gmail.com>
2023-12-14 10:44:26 +0700

    updated /api/tags

--- routes/tags.js
+++ routes/tags.js
@@ -5,27 +5,37 @@ const router = Router();
 
 //getAllTag - /api/tags
 router.get("/", async (req, res) => {
-    try {
-      const tags = await TagsModel.find();
-  
-      const response = {
-        status: "success",
-        message: "Tags retrieved successfully",
-        data: tags,
-      };
-  
-      res.json(response);
-    } catch (error) {
-      console.error("Error retrieving tags:", error);
-  
-      const errorResponse = {
-        status: "error",
-        message: "Failed to retrieve tags",
-        error: error.message,
-      };
-  
-      res.status(500).json(errorResponse);
-    }
-  });
+  try {
+    const tags = await TagsModel.find();
 
-module.exports = router
+    const libraryTags = tags.filter((tag) => tag.is_library);
+    const regularTags = tags.filter((tag) => !tag.is_library);
+
+    const response = {
+      status: "success",
+      message: "Tags retrieved successfully",
+      data: {
+        library: {
+          data: libraryTags,
+        },
+        tags: {
+          data: regularTags,
+        },
+      },
+    };
+    
+    res.json(response);
+  } catch (error) {
+    console.error("Error retrieving tags:", error);
+
+    const errorResponse = {
+      status: "error",
+      message: "Failed to retrieve tags",
+      error: error.message,
+    };
+
+    res.status(500).json(errorResponse);
+  }
+});
+
+module.exports = router;
