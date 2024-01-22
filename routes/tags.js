fd192efc04ae978bc683c316ec5c15c7d4c467ec
totoro <shockterraz@gmail.com>
2024-01-23 04:25:09 +0700

    Test new definition

--- routes/tags.js
+++ routes/tags.js
@@ -7,7 +7,7 @@ const router = Router();
 // GetAllTags - GET /api/tags
 router.get("/", async (req, res) => {
   try {
-    const tags = await TagsModel.find().select("-_id -__v -created_date");
+    const tags = await TagsModel.find().select("-__v -created_date");
 
     const libraryTags = tags.filter((tag) => tag.is_library);
 
@@ -64,9 +64,9 @@ router.get("/requested", verifyToken, async (req, res) => {
   }
 
   try {
-    const tags = await TagsModel.find({ is_library: false, status: "pending" }).select("-_id -__v -created_date");
+    const tags = await TagsModel.find({ is_library: false, status: "pending" }).select("-__v -created_date");
 
-    const libraryTags = await TagsModel.find({ is_library: true, status: "pending"}).select("-_id -__v -created_date");
+    const libraryTags = await TagsModel.find({ is_library: true, status: "pending"}).select("-__v -created_date");
 
     const response = {
       success: true,
@@ -81,6 +81,78 @@ router.get("/requested", verifyToken, async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Failed to retrieve tags list" });
+  }
+});
+
+//ApproveTag - PUT /api/tags/:id/approve
+router.put("/:id/approve", verifyToken, async (req, res) => {
+  userRole = req.decoded.role;
+
+  if (userRole !== "admin") {
+    return res.status(403).json({ success: false, message: "You are not authorized to access this route" });
+  }
+
+  const { id } = req.params;
+
+  try {
+    const tag = await TagsModel.findById(id);
+
+    if (!tag) {
+      return res.status(404).json({ success: false, message: "Tag not found" });
+    }
+
+    tag.status = "approved";
+
+    await tag.save();
+
+    const response = {
+      success: true,
+      message: "Tag approved successfully",
+      data: {
+        tag,
+      },
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Failed to approve tag" });
+  }
+});
+
+//DisapproveTag - PUT /api/tags/:id/disapprove
+router.put("/:id/disapprove", verifyToken, async (req, res) => {
+  userRole = req.decoded.role;
+
+  if (userRole !== "admin") {
+    return res.status(403).json({ success: false, message: "You are not authorized to access this route" });
+  }
+
+  const { id } = req.params;
+
+  try {
+    const tag = await TagsModel.findById(id);
+
+    if (!tag) {
+      return res.status(404).json({ success: false, message: "Tag not found" });
+    }
+
+    tag.status = "disapproved";
+
+    await tag.save();
+
+    const response = {
+      success: true,
+      message: "Tag disapproved successfully",
+      data: {
+        tag,
+      },
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Failed to disapprove tag" });
   }
 });
 
